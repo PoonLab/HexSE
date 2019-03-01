5a0463c2d95d34d9eb2d541cf38a4c07779f67ef
horaciobam <laura.baenam@gmail.com>
2019-03-01 13:23:07 -0500

    omega bias for parental rf

--- evol_rates.py
+++ evol_rates.py
@@ -1,19 +1,5 @@
 # Calculate evolution rates across the sequence
 
-transition_score = -5
-transversion_score = -1
-synonymous_score = 5
-nonsynonymous_score = 1
-rate_matrix = {
-    'A': {
-        'A': 0,
-        'C': transversion_score,
-        'T': transversion_score,
-        'G': transition_score,
-    },
-
-}
-
 def get_frequency_rates(seq):
     seq = list(seq)
     frequencies = {
@@ -26,24 +12,10 @@ def get_frequency_rates(seq):
     for nucleotide in frequencies:
         frequencies[nucleotide] = round((float(seq.count(nucleotide))/(len(seq))),2)
 
-
     return frequencies
 
 
-def get_syn_nonsyn_score():
-    return 1
-
-
-def get_tt_score(current, mutated):
-    return 1
-
-# bias = { 'A': [C,G,T],
-#          'C': [A,G,T],
-#          'G': [A,C,T],
-#          'T': [A,C,G],
-# }
-
-def evol_rates(seq, mu, bias, pi):
+def evol_rates(seq, mu, bias, pi, omega):
     """
     Generate rate vector from sequence given parameters.
     @param seq: the nucleotide sequence of length <L>.  Assumed to start and end in reading frame +0.
@@ -61,7 +33,7 @@ def evol_rates(seq, mu, bias, pi):
     """
 
     L = len(seq)
-    seq_rates = [(mu, mu, mu) for position in range(L)]  # initialize our list with baseline rate
+    seq_rates = [(mu, mu, mu) for position in range(L)]                      # initialize the list with baseline rates
     frequency_rates = get_frequency_rates(seq)
     # iterate over every nt in seq
     for position in range(L):
@@ -69,25 +41,40 @@ def evol_rates(seq, mu, bias, pi):
         nucleotide = seq[position]
 
         # 2. apply stationary frequencies (pi) to tuple given X
-        if pi is None:
-            pi = frequency_rates[nucleotide]
+        if pi is None:                                                       # If pi is no specified
+            pi = frequency_rates[nucleotide]                                 # use empirical frequencies
 
-        seq_rates[position] = tuple([pi*j for j in seq_rates[position]])
+        seq_rates[position] = tuple([pi*j for j in seq_rates[position]])     # Apply pi to every value in seq_rates according to the position
+
         # 3. apply biases to tuple
         biased_rates = []
 
-        for rate in range(3):
-            local_rates = seq_rates[position][rate] * bias[nucleotide][rate]
-            biased_rates.append(local_rates)
+        for rate in range(3):                                                      # Iterate over every item in the tuple
+            modified_rates = seq_rates[position][rate] * bias[nucleotide][rate]    # Create a new list with values of the tuple modified by bias
+            biased_rates.append(modified_rates)
 
-        formatted_biased_rates = [ '%.6f' % elem for elem in biased_rates]
-        seq_rates[position] = tuple(formatted_biased_rates)
+        seq_rates[position] = tuple(biased_rates)                        # Update seq_rates
 
     # 4. apply omega in parent reading frame
+    for position in range(L):
+        rf_0plus = omega['reading_frame0plus']                          # Omega values for parental reading frame
+        omega_rates = []
+        for rate in range(3):
+            modified_rates2 = seq_rates[position][rate] * rf_0plus[position][rate] #Apply omega values to seq_rates
+            omega_rates.append(modified_rates2)
 
-
+        seq_rates[position] = tuple(omega_rates)                         # Update seq_rates with modified values
 
     # 5. if alternate reading frame(s) is present, apply other omega(s)
 
-
+    #return modified_rates2
     return seq_rates
+
+def get_omega(seq, sub_matrix):
+    """
+    Generate a dictionary with possible reading frames(RF) in seq. Every RF is a key to a list where every position contains
+    rates of dN/dS substitution from the current nucleotide to the three possible mutations
+    :param seq: nucleotide sequence
+    :param sub_matrix: substitution matrix
+    :return omega: dictionary with all possible reading frames and dN/dS ratios for seq
+    """
--- evol_rates.test.py
+++ evol_rates.test.py
@@ -4,27 +4,23 @@ from evol_rates import get_frequency_rates
 # Substitution bias (transition-transversion) AC:0.001, AG:0.065, AT:0.002, CG:0.00001, CT:0.064, GT:0.00001
 # Values for this matrix were taken from: https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0070388
 
+# bias = { 'A': [C,G,T],
+#          'C': [A,G,T],
+#          'G': [A,C,T],
+#          'T': [A,C,G],
+# }
+
 bias = { 'A': [0.001,0.065,0.002],
          'C': [0.001,0.00001,0.064],
          'G': [0.065,0.00001,0.00001],
          'T': [0.002,0.064,0.00001], }
-
-
 mu = 1
-
-seq = 'AACTGAA'
-
-#print(evol_rates(seq, mu, bias,pi =  None))
-seq_rates = evol_rates(seq, mu, bias, pi=None)
-
+seq = 'AAA'
+pi = None
 
 # Hypothetical dictionary constructed for an 'AAA' (lys) sequence, where every mutation in first and second position are nonsyn
-omega = {'reading_frame1': [{'C': 0.001, 'G': 0.001, 'T':0.001}, {'C': 0.001, 'G': 0.001, 'T':0.001},{'C': 0.001, 'G': 0.3, 'T':0.001}],
-         'reading_frame2': [{'C': 0.001, 'G': 0.001, 'T':0.001}, {'C': 0.001, 'G': 0.001, 'T':0.001},{'C': 0.001, 'G': 0.3, 'T':0.001}]}
-print(omega)
+omega = {'reading_frame0plus': [[ 0.001,  0.001, 0.001], [ 0.001,  0.001, 0.001],[ 0.001,  0.3, 0.001]],
+         'reading_frame1plus': [[ 0.001,  0.001, 0.001], [ 0.001,  0.001, 0.001],[ 0.001,  0.3, 0.001]]}
 
-for key, value in omega.items():
-    second_ratio_dictionary = value[1]
-    c_ratio_number = second_ratio_dictionary['C']
-    print(c_ratio_number)
-    # print(value[1]['C'])
+print(evol_rates(seq, mu, bias, pi, omega))
+#seq_rates = evol_rates(seq, mu, bias, pi = None)
